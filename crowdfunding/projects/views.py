1c68bf1579ffbb0c1c95173e784512b2490fc492
Oriyan Nadav <oriyanadav5@gmail.com>
2023-08-05 09:34:28 +1000

    asks user to sign in to add

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -3,10 +3,11 @@ from rest_framework.response import Response
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
 
 
 class ProjectList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         projects = Project.objects.all()
