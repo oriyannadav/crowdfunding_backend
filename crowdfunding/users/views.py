398705ab249def7b3f6eb9cf2caf1f1719a0a0a9
Oriyan Nadav <oriyanadav5@gmail.com>
2023-08-11 12:20:54 +1000

    handle failed requests

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -15,10 +15,15 @@ class CustomUserList(APIView):
     def post(self, request):
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
-    
+            serializer.save(supporter=request.user)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class CustomUserDetail(APIView):
@@ -41,5 +46,12 @@ class CustomUserDetail(APIView):
             partial=True
         )
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            serializer.save(supporter=request.user)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
